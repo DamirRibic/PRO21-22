9b3a82e3724565a5a884844d792d75e2841e5f17
Damir Ribic <damir.ribic5290@gmail.com>
2021-10-13 10:07:08 +0200

    VSŠMobil

---
+++ VSŠMobil/VSŠMobil/Kupec60.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSŠMobil
+{
+    class Kupec60:Kupec
+    {
+        private int visjaTarifaPorabljeno;//koliko od 60min je ze porabljenih
+        public new void beleziKlic(int minute, int tip)
+        {
+            switch (tip)
+            {
+                case 1:
+                    stanje += minute * 0.2m;
+                    break;
+                case 2:
+                    int vMinute, nMinute;
+                    int šeVMinute = (visjaTarifaPorabljeno < 60) ? 60 - visjaTarifaPorabljeno : 0;
+                    if (minute > šeVMinute)
+                    {
+                        vMinute = šeVMinute;
+                        nMinute = minute - vMinute;
+
+                    }
+                    else
+                    {
+                        vMinute = minute;
+                        nMinute = 0;
+                    }
+                    stanje += vMinute * 0.05m + nMinute * 0.01m;
+                    visjaTarifaPorabljeno += vMinute;
+                    break;
+            }
+        }
+    }
+}
---
+++ VSŠMobil/VSŠMobil/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSŠMobil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kupec janez = new Kupec();
+            janez.Ime = "Janez Novak";
+            janez.beleziKlic(10, 1);
+            Console.WriteLine(janez.ToString());
+
+            Kupec60 alenka = new Kupec60();
+            alenka.Ime = "Alenka Prinčič";
+            alenka.beleziKlic(70,2);
+            
+            Console.WriteLine(alenka.ToString());
+            Console.WriteLine("metoda GetType " + alenka.GetType());
+            Console.WriteLine("metoda ToString " + alenka.ToString());
+            Console.ReadLine();
+
+        }
+
+    }
+}

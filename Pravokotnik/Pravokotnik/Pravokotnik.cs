85041e5bd1473e8916087ce9dde759a2d1408df4
Damir Ribic <damir.ribic5290@gmail.com>
2021-10-22 12:50:13 +0200

    pravokotnik

---
+++ Pravokotnik/Pravokotnik/Pravokotnik.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pravokotnik
+{
+    class Pravokotnik
+    {
+        private double širina;
+        private double višina;
+        private double ploščina;
+
+        public double Širina { get => širina; set => širina = value; }
+        public double Višina { get => višina; set => višina = value; }
+        public double Ploščina { get => višina*širina;}
+
+        public Pravokotnik()
+        {
+            širina = 0;
+            višina = 0;
+        }
+        public Pravokotnik(double Š, double V)
+        {
+            širina = Š;
+            višina = V;
+        }
+    public static bool operator ==(Pravokotnik p1, Pravokotnik p2)
+        {
+            if(p1.ploščina == p2.ploščina)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    public static bool operator !=(Pravokotnik p1, Pravokotnik p2)
+        {
+            if (p1 == p2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    public static bool operator >(Pravokotnik p1, Pravokotnik p2)
+        {
+            if (p1.ploščina > p2.ploščina)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public static bool operator <(Pravokotnik p1, Pravokotnik p2)
+        {
+            if (p1 < p2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public static bool operator <=(Pravokotnik p1, Pravokotnik p2)
+        {
+            if (p1 <= p2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public static bool operator >=(Pravokotnik p1, Pravokotnik p2)
+        {
+            if (p1 >= p2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
